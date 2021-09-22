3f45e9cda39de3938ac6ce087b899a0634cabc98
Aayushi-Shrawagi <74605322+Aayushi-Shrawagi@users.noreply.github.com>
2021-09-22 22:27:32 +0530

    Add files via upload

---
+++ Infocheck/assets/Home.js
@@ -0,0 +1,15 @@
+<div class="slide1">
+    get topnews trending fake etc. using api
+</div>
+<div class="slide2">
+    get topnews trending fake etc. using api
+</div>
+<div class="slide3">
+    get topnews trending fake etc. using api
+</div>
+<div class="slide4">
+    get topnews trending fake etc. using api
+</div>
+<div class="slide5">
+    get topnews trending fake etc. using api
+</div>
